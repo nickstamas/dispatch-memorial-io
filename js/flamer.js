16ba6f1eeecc798c90e076cac5484d40f05b639f
Nick Stamas <nicholas.stamas@gmail.com>
2013-11-16 09:51:31 -0500

    testing

--- js/flamer.js
+++ js/flamer.js
@@ -2,6 +2,10 @@ var canvas = null;
 var img = null;
 var ctx = null;
 var imageReady = false;
+var pi = 3.1415;
+var leftMax = 3*pi/4;
+var rightMax = pi/4;
+var rotationPercent = 0;
 
 function onload() { 
   canvas = document.getElementById('flameCanvas'); 
@@ -49,7 +53,12 @@ function redraw() {
   ctx.fillStyle = 'rgba(255,255,255,1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   if (imageReady) {
-    ctx.drawImage(img, frame*80, 0, 80, 160, canvas.width/2 - 48, canvas.height/2 - 48, 80, 160);
+    ctx.save();
+    ctx.translate( 100, 200 );
+    ctx.rotate( rotationPercent*leftMax );
+    ctx.translate( -40, -160 );
+    drawImageIOSFix(ctx, img, frame*80, 0, 80, 160, canvas.width/2 - 40, canvas.height - 160, 80, 160);
+    ctx.restore();
   }
 }
 
@@ -65,5 +74,45 @@ window.requestAnimFrame = (function() {
 })();
 
 gyro.startTracking(function(o) {
-    console.log(o);
+  rotationPercent = o.x/10;
 });
+
+/**
+ * Detecting vertical squash in loaded image.
+ * Fixes a bug which squash image vertically while drawing into canvas for some images.
+ * This is a bug in iOS6 devices. This function from https://github.com/stomita/ios-imagefile-megapixel
+ * 
+ */
+function detectVerticalSquash(img) {
+    var iw = img.naturalWidth, ih = img.naturalHeight;
+    var canvas = document.createElement('canvas');
+    canvas.width = 1;
+    canvas.height = ih;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0);
+    var data = ctx.getImageData(0, 0, 1, ih).data;
+    // search image edge pixel position in case it is squashed vertically.
+    var sy = 0;
+    var ey = ih;
+    var py = ih;
+    while (py > sy) {
+        var alpha = data[(py - 1) * 4 + 3];
+        if (alpha === 0) {
+            ey = py;
+        } else {
+            sy = py;
+        }
+        py = (ey + sy) >> 1;
+    }
+    var ratio = (py / ih);
+    return (ratio===0)?1:ratio;
+}
+
+/**
+ * A replacement for context.drawImage
+ * (args are for source and destination).
+ */
+function drawImageIOSFix(ctx, img, sx, sy, sw, sh, dx, dy, dw, dh) {
+    var vertSquashRatio = detectVerticalSquash(img);
+    ctx.drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh / vertSquashRatio);
+}
